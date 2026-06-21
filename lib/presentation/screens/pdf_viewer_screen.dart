c97613c5f491dd7578305a1e4f93493faf198b1b
medaad5682 <medaad5682@gmail.com>
2026-06-21 23:10:12 +0300

    Update pdf_viewer_screen.dart

--- lib/presentation/screens/pdf_viewer_screen.dart
+++ lib/presentation/screens/pdf_viewer_screen.dart
@@ -122,7 +122,7 @@ class _PdfViewerScreenState extends State<PdfViewerScreen> {
   }
 
   String _generateSecureToken() {
-    final random = Random.secure();
+    final random = math.Random.secure();
     final values = List<int>.generate(32, (i) => random.nextInt(256));
     return base64Url.encode(values);
   }
