8b76325e0e6ad9ae176f72d38193d5a3d6c83b27
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-07-02 19:07:58 +0300

    Update baseUrl in ApiConstants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String baseUrl = "https://medaad.aw478260.dpdns.org";
+  static const String baseUrl = "https://courses.aw478260.dpdns.org";
   static const String apiUrl = "$baseUrl/api";
 }
