a0f600296c5c04908018059f354bfac77b3ae600
medaad5682 <medaad5682@gmail.com>
2026-08-10 18:14:22 +0300

    Update api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String baseUrl = "https://courses.aw478260.dpdns.org";
+  static const String baseUrl = "https://medaad.online";
   static const String apiUrl = "$baseUrl/api";
 }
