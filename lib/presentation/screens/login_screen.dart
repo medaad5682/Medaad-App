ba7562cd31bd372f1be73544849777d2ac55d0f9
medaad5682 <medaad5682@gmail.com>
2026-08-14 00:27:32 +0300

    Update login_screen.dart

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -194,7 +194,7 @@ class _LoginScreenState extends State<LoginScreen> {
       if (response.statusCode == 200 && response.data['success'] == true) {
         AppState().updateFromInitData(response.data);
 
-        bool serverFreeMode = response.data['freeModeV6'] ?? false;
+        bool serverFreeMode = response.data['freeModeV7'] ?? false;
 
         // ⛔ إجبار الإغلاق للأندرويد
         if (Platform.isAndroid) {
@@ -259,7 +259,7 @@ class _LoginScreenState extends State<LoginScreen> {
           await box.put('email', response.data['user']['email']);
         }
 
-        bool serverFreeMode = response.data['freeModeV6'] ?? false;
+        bool serverFreeMode = response.data['freeModeV7'] ?? false;
 
         // ⛔ إجبار الإغلاق للأندرويد دائماً
         if (Platform.isAndroid) {
