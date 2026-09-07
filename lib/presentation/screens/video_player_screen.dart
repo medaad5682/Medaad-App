f5e24011a0a0850e8189f21de934b603a76a1441
medaad5682 <medaad5682@gmail.com>
2026-09-07 18:16:53 +0300

    Update video_player_screen.dart

--- lib/presentation/screens/video_player_screen.dart
+++ lib/presentation/screens/video_player_screen.dart
@@ -257,7 +257,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen>
 
       _player = Player(
         configuration: PlayerConfiguration(
-          bufferSize: _isWeakDevice ? 8 * 1024 * 1024 : 32 * 1024 * 1024,
+          bufferSize: _isWeakDevice ? 8 * 1024 * 1024 : 16 * 1024 * 1024,
           vo: 'gpu',
         ),
       );
